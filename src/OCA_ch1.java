d690a68ea0e24fd0e56612e86dd1e74671c563c0
bhautik <bhautiksakhiya005@gmail.com>
2022-06-07 17:26:46 +0530

    Loop Demo

---
+++ src/OCA_ch1.java
@@ -0,0 +1,19 @@
+class  Rocket{
+     void blastOff(){
+        System.out.println("bang");
+    }
+}
+public class OCA_ch1 extends Rocket{
+    public static void main(String[] args) {
+//          go();
+        new OCA_ch1().go();
+    }
+    void go(){
+        blastOff();
+        new Rocket().blastOff();
+    }
+//    private void blastOff(){
+//        System.out.println("sh-bang.");
+//    }
+
+}
